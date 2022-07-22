46c1a1fd261d36604ea563bebb34ed08ad8fe130
Guilherme Souza <guilhermebsoficial@gmail.com>
2022-07-22 01:01:24 -0300

    Update ex002.py
    Atualização do ex002 com informações extras adicionadas em forma de comentário.

--- CursoEmVideo/ex002.py
+++ CursoEmVideo/ex002.py
@@ -1,3 +1,7 @@
 nome = input('Qual é o seu nome? ')
 
 print('Olá, ', nome, '!! É um prazer em te conhecer!')
+
+# Também é possivel fazer a mesma coisa utilizando o seguinte comando:
+# print('Olá, {}!! É um prazer em te conhecer!'.format(nome))
+# O colchetes {} serão ocupados pela variavel que está em .format
